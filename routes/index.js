fe045738d38a80b0a82b9649ff3637048108d641
NickStepanenko <massaalonso@yandex.ru>
2015-12-26 20:22:31 +0800

    Admin page added

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Эксперт недвижемость' });
+  res.render('index', { title: 'Эксперт недвижимость' });
 });
 
 /* GET home page. */
-/*router.get('/apartment', function(req, res, next) {
-  res.render('pages/apartment', { object: obj });
-});*/
+router.get('/admin', function(req, res, next) {
+  res.render('pages/admin_main', { title: 'Admin main' });
+});
 
 /* GET home page. */
 /*router.get('/catalogue', function(req, res, next) {
